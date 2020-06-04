e4e26df0466012b408ff526510beff2f61dd8f44
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-04 10:24:19 +0700

    Update updatebot.php

--- updatebot.php
+++ updatebot.php
@@ -22,6 +22,7 @@ $gioitinh = 2;
 if ( !isUserExist($ID) ) { // nếu chưa tồn tại thì update lên sever
     $sql = "INSERT INTO `users` (`ID`, `trangthai`, `hangcho` ,`gioitinh`,`chatfuel`,`token`) VALUES (".$ID.", 0, 0 , $gioitinh,'$chatfuel','$token')";
    $info = mysqli_query($conn,$sql );
+  header("Location: thamgiabot.php?ID=$userID&token=$token");
   }
 
 mysqli_close($conn);
