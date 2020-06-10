58b70a99231acd5fd01f33bdbfc47737ff033772
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-10 19:08:40 +0700

    Update sendimage.php

--- sendimage.php
+++ sendimage.php
@@ -89,7 +89,7 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
     "id": "'.$userID.'"
   },
   "message":{
-    "text":"https://chathihi.herokuapp.com/index.php?&url='.$message.'"
+    "text":"'.$message.'"
     }
 }';
   $ch = curl_init($url);
