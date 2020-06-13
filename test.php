ec93ca8d5b3ef131dd0aef0d7efe15be2bff13ec
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-14 00:42:29 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
 	require_once 'config.php'; //lấy thông tin từ config
-	$conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME)or die("Lỗi kết nối tới cơ sở dữ liệu"); // kết nối data
+	$conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME)or die(echo 'lỗi'); // kết nối data
 	$sql = "SELECT ID, trangthai, gioitinh, hangcho, ketnoi FROM users";
 	$result = mysqli_query($conn, $sql);
 if (!$conn) {
