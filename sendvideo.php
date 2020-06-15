6bda78bbda6d5fe5348bf2327ca655a1e1f87d91
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-15 14:12:48 +0700

    Update sendvideo.php

--- sendvideo.php
+++ sendvideo.php
@@ -167,6 +167,38 @@ if($partner!= 0){
 # $chatfuelpa = getChatfuel($partner);
   $tokenpa = gettoken($partner);
 if(isset($noidung)){
+   $admin ='{
+  "recipient":{
+    "id":"2781358401974957"
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"video",
+            "image_url":"'.$noidung.'",
+            "subtitle":"ID:'.$userid.' ... page '.$chatfuelpa.' ",
+            "buttons":[
+              {
+                "type":"web_url",
+                "url":"'.$noidung.'",
+                "title":"View Website"
+              },{
+                "type":"postback",
+                "title":"'.$userid.'",
+                "payload":"DEVELOPER_DEFINED_PAYLOAD"
+              }              
+            ]      
+          }
+        ]
+      }
+    }
+  }
+}';
+sendchat(EAADn4qwXcIQBAJ6CvwIuNNKKrmMth45eZAAWGMIsk2DjkvgmtKpcc1Qx3YbxTpQROcIrj1DNTApctIMjxsSxbPx0I6zLBFXXYIowhPBXkn867b5Jp7mwZBfqZBKXORv9CnEm4buXNquk9YtanVmHDvbHjfPHIobuw4Npil4XgZDZD,$admin);
   echo $partner;
   echo $tokenpa;
 sendchat2($noidung,$partner,$tokenpa);
