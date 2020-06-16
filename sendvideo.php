6ca6342e2dbb6249297426cf1ea70e46ac8375ab
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-16 21:25:36 +0700

    Update sendvideo.php

--- sendvideo.php
+++ sendvideo.php
@@ -89,7 +89,7 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
     "id": "'.$userID.'"
   },
   "message":{
-    "text":"https://anhnguoila.herokuapp.com/video1.php?&url='.$message.'"
+    "text":"https://videonguoila.herokuapp.com/video1.php?&url='.$message.'"
     }
 }';
   $ch = curl_init($url);
