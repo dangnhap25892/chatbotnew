7cc6f3361fe3b366e2858e678487f29d75e4d16d
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-17 14:40:47 +0700

    Update index.php

--- index.php
+++ index.php
@@ -170,12 +170,12 @@ $hihi= "".$url."".$url2."".$url3."".$url18."".$url19."".$url6."".$url7."".$url8.
 		    <!-- https://admin.exoclick.com/qc3 -->
 	 <script type="application/javascript">
     var ad_idzone = "3895906",
-    ad_width = "728",
-    ad_height = "90"
+    ad_width = "468",
+    ad_height = "60"
 </script>
 <script type="application/javascript" src="https://a.exdynsrv.com/ads.js"></script>
 <noscript>
-    <iframe src="https://syndication.exdynsrv.com/ads-iframe-display.php?idzone=3895906&output=noscript&type=728x90" width="728" height="90" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+    <iframe src="https://syndication.exdynsrv.com/ads-iframe-display.php?idzone=3895906&output=noscript&type=468x60" width="468" height="60" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
 </noscript>
 	 <!-- https://admin.exoclick.com/qc3 -->
 	    </div>
