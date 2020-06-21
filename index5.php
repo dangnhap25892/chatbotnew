24e519dd30c531093aa309a2b23c95d55f405ad5
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-22 02:03:06 +0700

    Update index5.php

--- index5.php
+++ index5.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Người lạ gửi ảnh cho bạn</title>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 </head>
 
 <style>
