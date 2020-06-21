6377e3aa37f09b1aa6cca16edb664267b2ba818d
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-21 22:56:01 +0700

    Update xulybot.php

--- xulybot.php
+++ xulybot.php
@@ -349,8 +349,9 @@ if(isset($quick_reply)){
     die();
   }
   if($quick_reply=="timnam"){
-     header("Location: thamgiabotgt.php?ID=$userID&token=$token&gt=$quick_reply");
+     #header("Location: thamgiabotgt.php?ID=$userID&token=$token&gt=$quick_reply");
     #header("Location: updatebot.php?ID=$userID&token=$token&chatfuel=$chatpage&gt=0");
+    header("Location: updatebotgt.php?ID=$userID&token=$token&chatfuel=$chatpage&gt=$quick_reply");
     die();
   }
   if($quick_reply=="timnu"){
