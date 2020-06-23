f2b0fd039e9f1d6d2c24aa5f4b66cc68ad4fb6da
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-23 14:31:33 +0700

    Add files via upload

---
+++ chuyenanh.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Người lạ gửi ảnh cho bạn</title>
+	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+</head>
+
+<style>
+ img,
+    video {
+      max-width: 100%;
+    }
+
+    a#news {
+	    margin: 15px auto;
+	    display: block;
+	    text-decoration: none;
+	    color: #1c639e;
+	}
+</style>
+<body>
+	
+	
+	<!-- https://publishers.propellerads.com/  
+		<script>(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',3364458,document.body||document.documentElement)</script>
+		<script src="https://propu.sh/pfe/current/tag.min.js?z=3367736" data-cfasync="false" async></script>
+		<!-- https://publishers.propellerads.com/ -->
+	<!-- https://beta.propellerads.com/ -->
+	<script type="text/javascript">
+	atOptions = {
+		'key' : '5b8af646bd8073edd466651a45a066a8',
+		'format' : 'iframe',
+		'height' : 50,
+		'width' : 320,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/5b8af646bd8073edd466651a45a066a8/invoke.js"></scr' + 'ipt>');
+</script>
+	<!-- https://beta.propellerads.com/ -->
+	<script type="text/javascript">
+	atOptions = {
+		'key' : 'dc5121e073d0b5024c8851c4fa4579fc',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/dc5121e073d0b5024c8851c4fa4579fc/invoke.js"></scr' + 'ipt>');
+</script>
+	<!-- https://beta.propellerads.com/ -->
+     <h1 style="1">
+	     
+        <p>Ảnh người lạ đã gửi cho bạn</p>
+<?php	
+$url = $_GET['url'];
+$url2 = '&_nc_sid=';
+$url3 = $_GET['_nc_sid'];
+$url4 = '&_nc_oc=';
+$url5 = $_GET['_nc_oc'];
+$url6 = '&_nc_ad=';
+$url7 = $_GET['_nc_ad'];
+$url8 = '&_nc_cid=';
+$url9 = $_GET['_nc_cid'];
+$url10 = '&_nc_zor=';
+$url11 = $_GET['_nc_zor'];
+$url12 = '&_nc_ht=';
+$url13 = $_GET['_nc_ht'];
+$url14 = '&oh=';
+$url15 = $_GET['oh'];
+$url16 = '&oe=';
+$url17 = $_GET['oe'];
+$url18 = '&_nc_ohc=';
+$url19 = $_GET['_nc_ohc'];
+
+if (isset($url5)){
+    
+
+$hihi = "".$url."".$url2."".$url3."".$url4."".$url5."".$url6."".$url7."".$url8."".$url9."".$url10."".$url11."".$url12."".$url13."".$url14."".$url15."".$url16."".$url17."";
+}
+else{
+
+$hihi= "".$url."".$url2."".$url3."".$url18."".$url19."".$url6."".$url7."".$url8."".$url9."".$url10."".$url11."".$url12."".$url13."".$url14."".$url15."".$url16."".$url17."";
+}
+header("Location: https://anhnguoila.herokuapp.com/index5.php?&noidung=$hihi");
+die();
+?>
+
+	     
+	
+	     
+<img id="image" src= <?php echo "$hihi"?> >
+
+</h1>
+  <p>
+    <small>This is the content sent from the user in
+      <a href="https://m.me/102206461510133?ref=anh">Chatbot</a> on Facebook Messenger Platform
+      <br> People can talk with each other, send messages, photos, videos to each other and share their interesting stories.
+    </small>
+  </p>
+	<!-- https://beta.propellerads.com/ -->
+	<script async="async" data-cfasync="false" src="//pl15577867.passtechusa.com/ff3a32547adb4311f7dfa0aede354000/invoke.js"></script>
+<div id="container-ff3a32547adb4311f7dfa0aede354000"></div>
+	
+	<!-- https://beta.propellerads.com/ -->
+	<script type="application/javascript" data-idzone="3901452" src="https://a.exdynsrv.com/nativeads.js" ></script>
+	
+	<!-- https://beta.propellerads.com/ -->
+	<script type="application/javascript">
+    var ad_idzone = "3901062",
+    ad_width = "160",
+    ad_height = "600"
+</script>
+<script type="application/javascript" src="https://a.exdynsrv.com/ads.js"></script>
+<noscript>
+    <iframe src="https://syndication.exdynsrv.com/ads-iframe-display.php?idzone=3901062&output=noscript&type=160x600" width="160" height="600" scrolling="no" marginwidth="0" marginheight="0" frameborder="0"></iframe>
+</noscript>
+	<!-- https://beta.propellerads.com/ -->
+	<script type='text/javascript' src='//pl15578078.passtechusa.com/97/e1/52/97e152760174a26cf4e0934996daf82a.js'></script>
+	<!-- https://beta.propellerads.com/ -->
+</body>
+</html>
