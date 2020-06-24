7bf0a7856a863f3f5e8fbf38667158e968217a42
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-24 22:02:50 +0700

    Add files via upload

---
+++ hihi.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<?php
+	require_once 'tokenpage.php'; 
+	echo $hi;
+	echo $ha;
+	?>
+
+</body>
+</html>
---
+++ tokepage.php
@@ -0,0 +1,11 @@
+<?php
+
+$hi = "huhuhu";
+$ha = "hahaha";
+$h3 = "1";
+$h5 = "fgb";
+
+
+
+
+?>
