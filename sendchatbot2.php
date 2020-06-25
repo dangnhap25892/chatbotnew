4c3e44bcb02eea97803e5b69898fc34a98d19713
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-06-25 21:29:03 +0700

    Add files via upload

---
+++ sendchatbot2.php
@@ -0,0 +1,194 @@
+<?php
+require_once 'config.php'; //lấy thông tin từ config
+require_once ('tokenpage.php'); 
+$conn = mysqli_connect($DBHOST, $DBUSER, $DBPW, $DBNAME); // kết nối data
+$userid = $_GET['id'];
+$noidung = $_GET['noidung'];
+$token = $_GET['token'];
+ if ($conn->connect_error)  {
+     $jsonData ='{
+  "recipient":{
+    "id":"'.$userid.'"
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"Lỗi!",
+            "subtitle":"Hiện hệ thống đang lỗi xin vui lòng bạn quay lại sau.",
+          }
+        ]
+      }
+    }
+  }
+}';
+sendchat($token,$jsonData);
+die();
+}
+function isUserExist($userid) { //hàm kiểm tra xem user đã tồn tại chưa 
+  global $conn;
+  $result = mysqli_query($conn, "SELECT `ID` from `users` WHERE `ID` = $userid LIMIT 1");
+  $row = mysqli_num_rows($result);
+  return $row;
+}
+function ktidtime($userid) { //hàm kiểm tra xem user đã tồn tại chưa 
+  global $conn;
+  $result = mysqli_query($conn, "SELECT `ID` from `thoigian` WHERE `ID` = $userid LIMIT 1");
+  $row = mysqli_num_rows($result);
+  return $row;
+}
+
+////// Hàm Gửi JSON //////////
+
+function sendchat($token,$jsonData)
+{
+$url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
+
+  $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    $st=curl_exec($ch);
+
+  $errors = curl_error($ch);
+    $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    var_dump($errors);
+    var_dump($response);
+
+
+
+    curl_close($ch);
+    die();
+}
+function sendchat2($message,$userID,$token)
+{
+
+$url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
+ $jsonData ='{
+  "recipient":{
+    "id": "'.$userID.'"
+  },
+  "message":{
+    "text":"'.$message.'"
+    }
+}';
+  $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    $st=curl_exec($ch);
+
+    $errors = curl_error($ch);
+    $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    var_dump($errors);
+    var_dump($response);
+
+
+
+    curl_close($ch);
+    die();
+
+}
+//////// LẤY ID NGƯỜI CHÁT CÙNG ////////////
+function getRelationship($userid) {
+  global $conn;
+
+  $result = mysqli_query($conn, "SELECT ketnoi from users WHERE ID = $userid");
+  $row = mysqli_fetch_assoc($result);
+  $relationship = $row['ketnoi'];
+  return $relationship;
+}
+/// Lấy idpage ////
+function getidpage($partner) {
+  global $conn;
+
+  $result = mysqli_query($conn, "SELECT `chatfuel` from `users` WHERE `ID` = $partner");
+  $row = mysqli_fetch_assoc($result);
+  $relationship = $row['chatfuel'];
+  return $relationship;
+}
+$partner = getRelationship($userid);
+
+if($partner!= 0){
+# $chatfuelpa = getChatfuel($partner);
+  $idpage = getidpage($partner);
+ $page = tokenpage($idpage);
+ $tokenpa = $page[0];
+ $chatfuelpa = $page[1];
+ # $tokenpa = gettoken($partner);
+if(isset($noidung)){
+  echo $partner;
+  echo $tokenpa;
+ 
+sendchat2($noidung,$partner,$tokenpa);
+die();
+}
+
+     }
+else{
+  if ( !isUserExist($userid) ) {
+     $jsonData ='{
+  "recipient":{
+    "id": "'.$userid.'"
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"button",
+        "text":"Hiện hệ thống đang lỗi xin vui lòng bạn quay lại sau ít phút.\nVui lòng ấn vào sửa lỗi.",
+        "buttons":[
+          {
+            "type":"Postback",
+            "title":"Sửa lỗi",
+            "payload":"newchat"
+          },
+          {
+            "type":"Postback",
+            "title":"Thông tin chi tiết",
+            "payload":"thongtin"
+          }
+        ]
+      }
+    }
+  }
+}';
+sendchat($token,$jsonData);
+die();
+  }
+    else
+    {
+    mysqli_query($conn, "UPDATE `users` SET `hangcho` = 1 WHERE `ID` = $userid"); 
+  $jsonData ='{
+  "recipient":{
+    "id":"'.$userid.'"
+  },
+  "message":{
+    "attachment":{
+      "type":"template",
+      "payload":{
+        "template_type":"generic",
+        "elements":[
+           {
+            "title":"Đang tìm kiếm...",
+            "subtitle":"Bạn có thể gõ start để tìm kiếm nhanh hơn.",
+          }
+        ]
+      }
+    }
+  }
+}';
+sendchat($token,$jsonData);
+die();
+    }
+}
+mysqli_close($conn);
+die();
+?>
