3536f5da1bd13ad8d856510fc2df766b89ad6ab5
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-06-27 21:58:03 +0700

    Update xulybot.php

--- xulybot.php
+++ xulybot.php
@@ -582,7 +582,7 @@ if($message=='kiemtra2'){
   die();
  }
 if(isset($message)){
-  $hi = (rand(1,3));
+  $hi = (rand(1,5));
    $message = preg_replace('/\n+/', '\n', $message);
  
   if($hi == 1)
@@ -592,12 +592,18 @@ if(isset($message)){
 if($hi == 2)
 {
  header("Location: https://sendchatbot11.herokuapp.com/sendchatbot1.php?id=$userID&noidung=$message&token=$token");
-
 }
   if($hi == 3)
 {
  header("Location: https://sendchatbot11.herokuapp.com/sendchatbot2.php?id=$userID&noidung=$message&token=$token");
-
+}
+  if($hi == 4)
+{
+ header("Location: https://sendchatbot11.herokuapp.com/sendchatbot1.php?id=$userID&noidung=$message&token=$token");
+}
+  if($hi == 5)
+{
+ header("Location: https://sendchatbot11.herokuapp.com/sendchatbot2.php?id=$userID&noidung=$message&token=$token");
 }
 
  # header("Location: https://sendchatbot.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
