0294c6f2069d6ee490d2ebe0907f1ec5b725c69d
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-06-29 22:24:04 +0700

    Update thamgiabottim9x.php

--- thamgiabottim9x.php
+++ thamgiabottim9x.php
@@ -246,7 +246,7 @@ sendchat($token,$jsonData1);
         "elements":[
            {
             "title":"Người lạ đã tham gia cuộc trò chuyện",
-            "subtitle":"Gõ pp hoặc end chat để kết thúc.",
+            "subtitle":"Gõ pp hoặc end chat để kết thúc.\nBạn kết nối với id:'.$partner.'",
           }
         ]
       }
@@ -278,7 +278,7 @@ sendchat($tokenpa,$jsonData1);
         "elements":[
            {
             "title":"Người lạ đã tham gia cuộc trò chuyện",
-            "subtitle":"Gõ pp hoặc end chat để kết thúc.",
+            "subtitle":"Gõ pp hoặc end chat để kết thúc.\nBạn kết nối với id:'.$userid.'",
           }
         ]
       }
