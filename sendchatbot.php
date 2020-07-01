fef7ae11f79d4f864db1d2b2c63982da6db34201
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-02 00:34:40 +0700

    Update sendchatbot.php

--- sendchatbot.php
+++ sendchatbot.php
@@ -65,10 +65,10 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
     var_dump($response);
 
 
-
+*/
     curl_close($ch);
     die();
-    */
+    
 }
 function sendchat2($message,$userID,$token)
 {
@@ -86,6 +86,7 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
   curl_setopt($ch, CURLOPT_POSTFIELDS, html_entity_decode($jsonData));
   curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
   curl_exec($ch);
+ curl_close($ch);
   die();
    /*
     curl_setopt($ch, CURLOPT_POST, 1);
