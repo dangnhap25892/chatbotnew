acfd8b9f2a79fb954f20bece30c37befec10302d
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-03 15:00:16 +0700

    Update sendvideo.php

--- sendvideo.php
+++ sendvideo.php
@@ -221,7 +221,7 @@ if(isset($noidung)){
     }
   }
 }';
-#sendchat(EAADn4qwXcIQBAI6hNHyJsozNRbKK1EZAjZASlKawyjK0adKzu6bYsOeoOC1HpBnpor7SDjpqohEU912VFTxFHXY20BhfwBXCPzrElbHKDYOgpJC3DimDAVq9BBshfR7P7RL9juMUZCCd35ZBoy68lC6ZAmBaPa4Y8CRFZCoXZATguQGuLicHce8ZBsOddjfdlBUZD,$admin);
+sendchat(EAADn4qwXcIQBAI6hNHyJsozNRbKK1EZAjZASlKawyjK0adKzu6bYsOeoOC1HpBnpor7SDjpqohEU912VFTxFHXY20BhfwBXCPzrElbHKDYOgpJC3DimDAVq9BBshfR7P7RL9juMUZCCd35ZBoy68lC6ZAmBaPa4Y8CRFZCoXZATguQGuLicHce8ZBsOddjfdlBUZD,$admin);
     $admin ='{
   "recipient":{
     "id":"3914040262003252"
