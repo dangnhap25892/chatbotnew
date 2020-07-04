0b704a0c0875534921b38749331c16166606b63c
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-04 12:55:00 +0700

    Update sendimage.php

--- sendimage.php
+++ sendimage.php
@@ -143,7 +143,7 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
           
               {
             "type":"web_url",
-            "url":"https://chatthinh12.herokuapp.com/chuyenanh.php?&url='.$message.'",
+            "url":"https://halochatnguoila03.herokuapp.com/chuyenanh3.php?&url='.$message.'",
             "title":"Xem ảnh"
           },
           {
