8519e2e8da065f60f17e1f797e9bec38e3b6491e
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-07 13:03:20 +0700

    Update xulybot.php

--- xulybot.php
+++ xulybot.php
@@ -72,7 +72,7 @@ if($getstart['postback']['payload']=="chiase" ){
      'id': $userID
    },
    'message':{
-     'text': 'https://m.me/Halochat.VN1?ref=".$userID."'
+     'text': 'https://m.me/halochatvn2?ref=".$userID."'
      }
  }";
  sendchat($token,$jsonData);
