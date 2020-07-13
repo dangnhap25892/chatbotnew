72339b9836a9513b67d2d259fa83558f1a68c19e
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-13 23:28:28 +0700

    Update xulybot.php

--- xulybot.php
+++ xulybot.php
@@ -732,22 +732,22 @@ if(isset($message)){
   if($hi == 1)
 {
  #header("Location: https://sendchatbot10.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
- header("Location: https://halochatbot1sendchat4.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");   
+ header("Location: https://halochatbot1sendchat2.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");   
 }
 if($hi == 2)
 {
  #header("Location: https://sendchatbot10.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
-  header("Location: https://halochatbot1sendchat4.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
+  header("Location: https://halochatbot1sendchat2.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
 }
   if($hi == 3)
 {
  #header("Location: https://sendchatbot10.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
-    header("Location: https://halochatbot1sendchat4.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
+    header("Location: https://halochatbot1sendchat2.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
 }
   if($hi == 4)
 {
  #header("Location: https://sendchatbot10.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
-    header("Location: https://halochatbot1sendchat4.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
+    header("Location: https://halochatbot1sendchat1.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
 }
   if($hi == 5)
 {
