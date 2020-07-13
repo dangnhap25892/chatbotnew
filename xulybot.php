23d91926f06936004ad4140c1ba0aba103b4475f
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-13 22:28:41 +0700

    Update xulybot.php

--- xulybot.php
+++ xulybot.php
@@ -747,12 +747,12 @@ if($hi == 2)
   if($hi == 4)
 {
  #header("Location: https://sendchatbot10.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
-    header("Location: https://halochatbot1sendchat1.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
+    header("Location: https://halochatbot1sendchat4.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
 }
   if($hi == 5)
 {
  #header("Location: https://sendchatbot11.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
-    header("Location: https://halochatbot1sendchat1.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
+    header("Location: https://halochatbot1sendchat4.herokuapp.com/sendchatbot.php?id=$userID&noidung=$message&token=$token");
 }
 
 
