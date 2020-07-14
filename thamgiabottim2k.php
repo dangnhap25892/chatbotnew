588d9e8499093b8a88695fb5eee3a5354a5a8ca4
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-14 23:37:00 +0700

    Update thamgiabottim2k.php

--- thamgiabottim2k.php
+++ thamgiabottim2k.php
@@ -103,6 +103,10 @@ function sendchat($token,$jsonData)
 $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
 
   $ch = curl_init($url);
+     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+  curl_exec($ch);
+     /*
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
     curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
@@ -117,6 +121,7 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
 
 
     curl_close($ch);
+    */
 }
 
 ///// hàm kiểm tra hàng chờ ///////
