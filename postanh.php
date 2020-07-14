a88b19b8af125bd234a234383c1dfb26ee5470f5
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-15 03:04:26 +0700

    Update postanh.php

--- postanh.php
+++ postanh.php
@@ -80,7 +80,7 @@ setTimeout("auto_sub()",0);
 
 <body onLoad="auto_sub1();">
 <form name="form" method="post" action="https://anhnguoila00.herokuapp.com/index.php">
-	<input type="radio" name="fname" value=<?php echo "$hihi"?> >
+	<input type="hidden" name="fname" value=<?php echo "$hihi"?> >
 </form>
 <!--
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
