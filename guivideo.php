dbd97dee7b7d9f8191df9aa9557c91f7212c063b
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-16 23:18:25 +0700

    Update guivideo.php

--- guivideo.php
+++ guivideo.php
@@ -63,7 +63,7 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
           
               {
             "type":"web_url",
-            "url":"https://anhnguoila02.herokuapp.com/postvideonew.php?&url='.$message.'",
+            "url":"https://halochatanh1.herokuapp.com/postvideonew.php?&url='.$message.'",
             "title":"Xem video"
           },
          
