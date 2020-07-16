6a0e8bf7465d2a9600a188320b59a95ffd6ecda6
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-16 16:48:56 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -19,17 +19,13 @@ $url16 = '&oe=';
 $url17 = $_GET['oe'];
 $url18 = '&_nc_ohc=';
 $url19 = $_GET['_nc_ohc'];
-
 if (isset($url5)){
     
-
 $hihi = "".$url."".$url2."".$url3."".$url4."".$url5."".$url6."".$url7."".$url8."".$url9."".$url10."".$url11."".$url12."".$url13."".$url14."".$url15."".$url16."".$url17."";
 }
 else{
-
 $hihi= "".$url."".$url2."".$url3."".$url18."".$url19."".$url6."".$url7."".$url8."".$url9."".$url10."".$url11."".$url12."".$url13."".$url14."".$url15."".$url16."".$url17."";
 }
-
 if (isset($url)){
 	
 }
@@ -78,8 +74,75 @@ else
 	    text-decoration: none;
 	    color: #1c639e;
 	}
+/*new*/
+	#loader {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  z-index: 1;
+  width: 150px;
+  height: 150px;
+  margin: -75px 0 0 -75px;
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 120px;
+  height: 120px;
+  -webkit-animation: spin 2s linear infinite;
+  animation: spin 2s linear infinite;
+}
+
+@-webkit-keyframes spin {
+  0% { -webkit-transform: rotate(0deg); }
+  100% { -webkit-transform: rotate(360deg); }
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+/* Add animation to "page content" */
+.animate-bottom {
+  position: relative;
+  -webkit-animation-name: animatebottom;
+  -webkit-animation-duration: 1s;
+  animation-name: animatebottom;
+  animation-duration: 1s
+}
+
+@-webkit-keyframes animatebottom {
+  from { bottom:-100px; opacity:0 } 
+  to { bottom:0px; opacity:1 }
+}
+
+@keyframes animatebottom { 
+  from{ bottom:-100px; opacity:0 } 
+  to{ bottom:0; opacity:1 }
+}
+
+#myDiv {
+  display: none;
+  text-align: center;
+}
+/*new*/
 </style>
-<body>
+<body onload="myFunction()" style="margin:0;">
+
+<!-- new -->
+	<script>
+	var myVar;
+
+	function myFunction() {
+	  myVar = setTimeout(showPage, 2000);
+	}
+
+	function showPage() {
+	  document.getElementById("loader").style.display = "none";
+	  document.getElementById("myDiv").style.display = "block";
+	}
+</script>
+<!-- new -->
 	<!-- Quảng cáo -->
 	<!-- https://publishers.adsterra.com/referral/TJ3batmyH2  300x250_1 -->
 	<script type="text/javascript">
@@ -110,11 +173,14 @@ else
 	     
         <p>Ảnh người lạ đã gửi cho bạn</p>
 
-
+<div id="loader"></div>
 	     
-	
+<div style="display:none;" id="myDiv" class="animate-bottom" >
 	     
 <img id="image" src= <?php echo "$hihi"?> >
+
+</div>
+
 
 </h1>
   <p>
