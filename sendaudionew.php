54717e25415c446325126163f870b7c6b78a73d1
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-16 14:20:44 +0700

    Update sendaudionew.php

--- sendaudionew.php
+++ sendaudionew.php
@@ -164,7 +164,7 @@ if(isset($noidung)){
   echo $partner;
   echo $tokenpa;
 #sendchat3($message,$partner,$tokenpa);
-  header("Location: https://sendimage002.herokuapp.com/guiviod.php?id=$partner&noidung=$message&token=$tokenpa");
+  header("Location: https://sendvoid001.herokuapp.com/guiviod.php?id=$partner&noidung=$noidung&token=$tokenpa");
 die();
 }
 
