03d0685cb636b947f8a478444da036ce30ed3a0e
dangnhap2589 <65329898+dangnhap2589@users.noreply.github.com>
2020-07-17 16:57:19 +0700

    Update linkduphong.php

--- linkduphong.php
+++ linkduphong.php
@@ -129,8 +129,6 @@ else
 </style>
 <body onload="myFunction()" style="margin:0;">
 	<!-- https://a-ads.com?partner=1432547 -->
-<iframe data-aa="1432547" src="//ad.a-ads.com/1432547?size=240x400" scrolling="no" style="width:240px; height:400px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
-<!-- https://a-ads.com?partner=1432547 -->
 	<iframe data-aa="1432553" src="//ad.a-ads.com/1432553?size=300x250" scrolling="no" style="width:300px; height:250px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
 <!-- https://a-ads.com?partner=1432547 -->
 <!-- new -->
@@ -182,6 +180,9 @@ function myFunction1() {
       <br> People can talk with each other, send messages, photos, videos to each other and share their interesting stories.
     </small>
   </p>
+	<!-- https://a-ads.com?partner=1432547 -->
+<iframe data-aa="1432547" src="//ad.a-ads.com/1432547?size=240x400" scrolling="no" style="width:240px; height:400px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
+<!-- https://a-ads.com?partner=1432547 -->
 	
 	
 </body>
