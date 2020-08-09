8607dd61f2be9f3a24086bd8efbeaba8ebbbc158
dangnhap25892 <66126263+dangnhap25892@users.noreply.github.com>
2020-08-09 14:39:36 +0700

    Update sendchat.php

--- sendchat.php
+++ sendchat.php
@@ -4,6 +4,46 @@ $partner = $_GET['id'];
 $noidung = $_GET['noidung'];
 $tokenpa = $_GET['token'];
 
+
+if (preg_match('/sex/', $noidung)||preg_match('/xxx/', $noidung)||preg_match('/dâm/', $noidung)||preg_match('/Sex/', $noidung)||preg_match('/Xxx/', $noidung)||preg_match('/Dâm/', $noidung)) {
+    echo 'true';
+  $jsonData ='{
+  "recipient":{
+    "id":"'.$partner.'"
+  },
+  "messaging_type": "RESPONSE",
+  "message":{
+    "text": "Người lạ gửi chat có chứa nội nhạy cảm bạn có muốn tố cáo đối phương.",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Tố cáo và kết thúc",
+        "payload":"endchat",
+      },{
+        "content_type":"text",
+        "title":"Không.",
+        "payload":"Khong",
+      }
+      
+    ]
+  }
+}';
+    sendchat($tokenpa,$jsonData);
+}
+
+//new
+$noidung = str_replace("sex","s.e.x",$noidung);
+$noidung = str_replace("xxx","x.x.x",$noidung);
+$noidung = str_replace("dâm","d.â.m",$noidung);
+$noidung = str_replace("cặc","c.ặ.c",$noidung);
+$noidung = str_replace("lồn","l.ồ.n",$noidung);
+$noidung = str_replace("Sex","s.e.x",$noidung);
+$noidung = str_replace("Xxx","x.x.x",$noidung);
+$noidung = str_replace("Dâm","d.â.m",$noidung);
+$noidung = str_replace("Cặc","c.ặ.c",$noidung);
+$noidung = str_replace("Lồn","l.ồ.n",$noidung);
+//new
+
 function sendchat($token,$jsonData)
 {
 $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
@@ -46,30 +86,6 @@ $url = "https://graph.facebook.com/v7.0/me/messages?access_token=$token";
    
 }
 sendchat2($noidung,$partner,$tokenpa);
-if (preg_match('/sex/', $noidung)||preg_match('/xxx/', $noidung)||preg_match('/dâm/', $noidung)||preg_match('/Dâm/', $noidung)||preg_match('/Sex/', $noidung)||preg_match('/Xxx/', $noidung)) {
-    echo 'true';
-  $jsonData ='{
-  "recipient":{
-    "id":"'.$partner.'"
-  },
-  "messaging_type": "RESPONSE",
-  "message":{
-    "text": "Người lạ gửi chat có chứa nội nhạy cảm bạn có muốn tố cáo đối phương.",
-    "quick_replies":[
-      {
-        "content_type":"text",
-        "title":"Tố cáo và kết thúc",
-        "payload":"endchat",
-      },{
-        "content_type":"text",
-        "title":"Không.",
-        "payload":"Khong",
-      }
-      
-    ]
-  }
-}';
-    sendchat($tokenpa,$jsonData);
-}
+
 die();
 ?>
